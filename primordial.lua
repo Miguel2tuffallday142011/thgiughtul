b1f3a9845aa2a3a0fd08c3d13a2529d077fd970f
Miguel2tuffallday142011 <tumbleoutofbed@outlook.com>
2026-06-26 11:48:30 -0400

    Update primordial.lua

--- primordial.lua
+++ primordial.lua
@@ -537,7 +537,7 @@ function PrimordialUI:CreateWindow(config)
                     UDim2.new(0.5, -4, 0, 0), UDim2.new(0,0,0,0), Theme.BG)
                 stLeftHolder.LayoutOrder = 1
                 stLeftHolder.AutomaticSize = Enum.AutomaticSize.Y
-                MakeListLayout(stLeftHolder, Enum.FillDirection.Vertical, 20)
+                MakeListLayout(stLeftHolder, Enum.FillDirection.Vertical, 12)
                 SubTab._leftHolder = stLeftHolder
 
                 -- Right column: same size, hidden until used
@@ -546,7 +546,7 @@ function PrimordialUI:CreateWindow(config)
                 stRightHolder.LayoutOrder = 2
                 stRightHolder.AutomaticSize = Enum.AutomaticSize.Y
                 stRightHolder.Visible = false
-                MakeListLayout(stRightHolder, Enum.FillDirection.Vertical, 20)
+                MakeListLayout(stRightHolder, Enum.FillDirection.Vertical, 12)
                 SubTab._rightHolder = stRightHolder
                 SubTab._colHolder = stColHolder
                 SubTab._hasTwoCols = false
@@ -612,7 +612,7 @@ function PrimordialUI:CreateWindow(config)
                     box.Parent = holder
                     MakeCorner(box, 6)
                     MakePadding(box, 10, 10, 10, 10)
-                    MakeListLayout(box, Enum.FillDirection.Vertical, 12)
+                    MakeListLayout(box, Enum.FillDirection.Vertical, 8)
 
                     -- Section title
                     local titleRow = MakeFrame(box, UDim2.new(1,0,0,16), nil, Theme.BGTertiary)
